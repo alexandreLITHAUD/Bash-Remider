f5f6a184540a6aa7777f64607268cceec5881a04
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2023-08-10 17:19:23 +0200

    add start

--- sh-rem.sh
+++ sh-rem.sh
@@ -14,17 +14,63 @@ UNCHECKED='\u2610'
 
 FILE_NAME='.sh-rem-data.yaml' #default .sh-rem-data.yaml
 
-
-addValue(){ # Message  [Importance] default 3 
-    if [$#=="1"]; then
-        new_uuid=(uuidgen)
-        yq eval ".I3 += {\"$new_uuid\": \"$1\"}" $FILE_NAME -i
+# Work but - problem sometimes
+addValue(){ # Message  [Importance] default 3
+    new_uuid=$(uuidgen)
+    if [ $# -eq 1 ]; then
+        yq eval ".I3 += [{\"$new_uuid\": {\"$1\"}]" $FILE_NAME -i
     else
-    
+        yq eval ".I$2 += [{\"$new_uuid\": \"$1\"}]" $FILE_NAME -i
     fi;
 }
 
+printValues(){
+    echo "on fait des test ici !!"
 
+    # PRINT I1 FIRST (CHECK BEFORE UNCHECKED)
+    # PRINT IN FIRST (CHECK BEFORE UNCHECKED)
+    # PRINT IN+1 FIRST (CHECK BEFORE UNCHECKED)
+}
+
+helper(){
+    echo "TODO"
+    exit
+}
+
+
+case "$1" in
+add) 
+    if [ $# -eq 3 ]; then
+        addValue $2 $3
+    elif [ $# -eq 2 ]; then
+        addValue $2
+    else
+        helper "add"
+    fi;;
+list)
+    SHOW_VALUE=$(yq '.show' $FILE_NAME)
+    if [ $SHOW_VALUE = "false" ]; then
+        exit        
+    fi;
+    if [ $# -eq 2 ]; then
+        printValues $2
+    elif [ $# -eq 1 ]; then
+        printValues
+    else
+        helper "list"
+    fi;;
+check) echo "TODO";;
+prune) echo "TODO";;
+del) echo "TODO";;
+prune) echo "TODO";;
+config) echo "TODO";;
+modify) echo "TODO";;
+link) echo "TODO";;
+help) helper "help";;
+esac
+
+
+exit
 
 # echo -e "${BOLD}This text is bold.${RESET}"
 # echo -e "${UNDERLINE}This text is underlined.${RESET}"
